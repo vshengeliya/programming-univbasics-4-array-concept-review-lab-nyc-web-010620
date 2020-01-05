57cb5f911af6494e13d5b1c57ce86ab46160217a
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-05 04:48:36 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,12 +4,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  i=0
-  while i<array.length do
-  puts array[i].max
-  i += 1
-  end
-  find_max_value
+  puts array.max
 end
 
 # counter=0
